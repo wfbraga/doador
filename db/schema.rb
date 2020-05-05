5d6091daa5e542f4dbc30e82072e0e7877cab6c0
Wagner Braga <wfbraga@hotmail.com>
2020-05-05 15:55:40 -0300

    inicando categorias

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_175712) do
+ActiveRecord::Schema.define(version: 2020_05_05_185445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categoria", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entradas", force: :cascade do |t|
     t.date "data"
